22a54281e7f4fff17c57082cebc0935cb3d8cd54
Takahashi Rina <19r.takahashi90@gmail.com>
2018-02-22 09:41:49 +0900

    add tanto.rb spec

---
+++ tanto_spec.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+require_relative 'tanto'
+
+describe Tanto do
+    let(:tanto) { Tanto.new }
+
+    example '担当の給与は基本給と一緒。基本給が100なら給料も100' do
+        expect(tanto.calculate_salary(100)).to eq 100
+    end
+end
