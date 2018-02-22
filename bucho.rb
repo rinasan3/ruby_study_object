bd93de6ea7f1638ea3b6268648dad390820ca74c
Takahashi Rina <19r.takahashi90@gmail.com>
2018-02-22 09:33:38 +0900

    modify bucho.rb

--- bucho.rb
+++ bucho.rb
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 require_relative './shain.rb'
 
-# Bucho class inherited from Shain class for lesson 3.3
+# Bucho class inherited from Shain class for lesson 3.5
 class Bucho < Shain
   @@kihonkyu = @@kihonkyu * 3
   def standup
     puts '部長がだるそうに立ちました。'
   end
-  def kihonkyu
-    puts '基本給は#{@@kihonkyu}円です'
+
+  def calculate_salary(kihonkyu)
+    kihonkyu * 3
   end
 end
