69a6d0f9f9459220066f1fbdd8b9b7b26b0d4125
Takahashi Rina <19r.takahashi90@gmail.com>
2018-02-22 09:31:17 +0900

    modify tanto.rb

--- tanto.rb
+++ tanto.rb
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 require_relative './shain.rb'
 
-# Tanto class inherited from Shain class for lesson 3.3
+# Tanto class inherited from Shain class for lesson 3.5
 class Tanto < Shain
   def standup
     puts '担当は慌てて起立しました。'
   end
 
-  def kihonkyu
-    puts "基本給は#{@@kihonkyu}円です"
+  def calculate_salary(kihonkyu)
+    kihonkyu
   end
 end
