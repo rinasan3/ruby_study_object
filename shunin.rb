fb7ff9ff978fcbec933fb299fbe229a056e5bbbc
Takahashi Rina <19r.takahashi90@gmail.com>
2018-02-22 09:33:07 +0900

    modify shunin.rb

--- shunin.rb
+++ shunin.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 require_relative './shain.rb'
 
-# Shunin class inherited form Shain for lesson 3.3
+# Shunin class inherited form Shain for lesson 3.5
 class Shunin < Shain
   @@kihonkyu = @@kihonkyu * 2 + 1
   def standup
     puts '主任が素早く立ちました。'
   end
 
-  def kihonkyu
-    puts '基本給は#{@@kihonkyu}円です'
+  def calculate_salary(kihonkyu)
+    kihonkyu * 2 + 1
   end
 end
