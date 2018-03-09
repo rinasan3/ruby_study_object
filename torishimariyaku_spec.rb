6c561d451b13a3b4336d023b7937aff927361ac6
Takahashi Rina <19r.takahashi90@gmail.com>
2018-03-09 15:53:16 +0900

    modify torishimariyaku

--- torishimariyaku_spec.rb
+++ torishimariyaku_spec.rb
@@ -1,10 +1,10 @@
 # coding: utf-8
 require_relative 'bucho'
 
-describe Bucho do
-    let(:bucho) { Bucho.new }
+describe Torishimariyaku do
+    let(:torishimariyaku) { Torishimariyaku.new }
 
-    example '部長の給料は基本給の3倍なので、基本給が100なら給料は300' do
-        expect(bucho.calculate_salary(100)).to eq 300
+    example '取締役の給料は基本給の3倍なので、基本給が100なら給料は402' do
+        expect(torishimariyaku.calculate_salary(100)).to eq 402
     end
 end
